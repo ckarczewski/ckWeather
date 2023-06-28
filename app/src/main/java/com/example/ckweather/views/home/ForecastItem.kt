12780633cdf99c2eec5df97004eb77a13a2235b7
Czarek Karczewski <keracz1997@gmail.com>
2023-06-28 20:30:56 +0200

    forecastitem element add

--- app/src/main/java/com/example/ckweather/views/home/ForecastItem.kt
+++ app/src/main/java/com/example/ckweather/views/home/ForecastItem.kt
@@ -1,2 +1,31 @@
 package com.example.ckweather.views.home
 
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun ForecastItem(date: String, icon: ImageVector, temperature: String){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = date,
+            color = Color.White
+        )
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color.Unspecified
+        )
+        Text(
+            text = temperature,
+            color = Color.White
+        )
+    }
+}
