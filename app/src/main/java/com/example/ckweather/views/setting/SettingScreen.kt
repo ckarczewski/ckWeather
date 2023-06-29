4a2a8f7627ca1654e322f2bb5ea4a4c2b8b0cc09
Czarek Karczewski <keracz1997@gmail.com>
2023-06-29 23:58:56 +0200

    add setting screen

--- app/src/main/java/com/example/ckweather/views/setting/SettingScreen.kt
+++ app/src/main/java/com/example/ckweather/views/setting/SettingScreen.kt
@@ -3,17 +3,23 @@ package com.example.ckweather.views.setting
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.ckweather.MainViewModel
 import com.example.ckweather.R
+import com.example.ckweather.helpers.helpers
+import com.example.ckweather.views.location.LocationViewModel
 
 
 //@RequiresApi(Build.VERSION_CODES.N)
@@ -22,17 +28,110 @@ fun SettingScreen (
     navController: NavHostController
 ) {
     Log.d("setting screen","dupa2")
-    IconButton(
-        modifier = Modifier
-        ,onClick = {
-            navController.navigate("home")
-        }) {
-        Icon(
-            imageVector = ImageVector.vectorResource(id = R.drawable.ic_baseline_arrow_circle_left_24),
-            contentDescription = null,
-            modifier = Modifier
-                .size(32.dp),
-            tint = Color(0xFFF5F5F5)
-        )
+
+    var tempUnit by remember { mutableStateOf(helpers.getTemperatureUnit()) }
+    var mainViewModel: MainViewModel = viewModel();
+    var locationViewModel: LocationViewModel = viewModel()
+    var expand by remember { mutableStateOf(false) }
+    val suggestions = helpers.TempUnits.values()
+    Column() {
+        IconButton(
+            modifier = Modifier.offset(x = 10.dp, y = 10.dp)
+            ,onClick = {
+                navController.navigate("home")
+            }) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_baseline_arrow_circle_left_24),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(36.dp),
+                tint = Color(0xFF1b1e23)
+            )
+        }
+        Row() {
+            Text(
+                modifier = Modifier.padding(all = 15.dp),
+                text = "Jednostka temperatury: ",
+                color = Color.Black,
+                fontSize = 18.sp
+            )
+
+            OutlinedButton(
+                border = BorderStroke(2.dp, Color.White),
+                shape = RoundedCornerShape(25.dp),
+                onClick = { expand = true },
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+            ) {
+
+                Log.i("test", tempUnit)
+
+
+                when(tempUnit){
+                    "°C" -> {
+                        Text(
+                            text = "°C",
+                            fontSize = 18.sp,
+                            color = Color.Black
+                        )
+                    }
+
+                    "K" -> {
+                        Text(
+                            text = "K",
+                            fontSize = 18.sp,
+                            color = Color.Black
+                        )
+                    }
+
+                    "°F" -> {
+                        Text(
+                            text = "°F",
+                            fontSize = 18.sp,
+                            color = Color.Black
+                        )
+                    }
+                }
+
+            }
+
+            DropdownMenu(
+                expanded = expand,
+                onDismissRequest = { expand = false }
+            ) {
+                suggestions.forEach { label ->
+                    DropdownMenuItem(onClick = {
+                        mainViewModel.changeTemperatureUnit(label)
+                        tempUnit = when(label){
+                            helpers.TempUnits.C -> "°C"
+                            helpers.TempUnits.K -> "K"
+                            helpers.TempUnits.F -> "°F"
+                        }
+                        expand = false
+                    }) {
+                        Text(text = label.toString())
+                    }
+                }
+            }
+        }
+        Row() {
+            OutlinedButton(
+                border = BorderStroke(2.dp, Color.White),
+                shape = RoundedCornerShape(25.dp),
+                onClick = {
+                    locationViewModel.updateAllWeatherDb()
+                },
+                modifier = Modifier
+                    .padding(horizontal = 20.dp)
+                    .fillMaxWidth()
+            ){
+                Text(
+                    text = "Aktualizacja danych",
+                    fontSize = 18.sp,
+                    color = Color.Black
+                )
+            }
+        }
+
     }
 }
