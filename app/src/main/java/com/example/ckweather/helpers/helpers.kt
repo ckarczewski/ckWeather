fa483ab4b2741ebdc2071245bb37cfc09dff9d85
Czarek Karczewski <keracz1997@gmail.com>
2023-06-29 13:08:55 +0200

    add weather description helper function

--- app/src/main/java/com/example/ckweather/helpers/helpers.kt
+++ app/src/main/java/com/example/ckweather/helpers/helpers.kt
@@ -3,9 +3,11 @@ package com.example.ckweather.helpers
 import com.example.ckweather.R
 
 object helpers {
+    enum class TempUnits {C,F,K}
+
     fun getWeatherIcon(id: Int): Int {
         val idStr = id.toString()[0].toString()
-        when(idStr){
+        when (idStr) {
             "2" -> {
                 return R.drawable.thunderstorm_48px
             }
@@ -22,16 +24,57 @@ object helpers {
                 return R.drawable.cloudy_48px
             }
             "8" -> {
-                if (id==800){
+                if (id == 800) {
                     return R.drawable.sunny_48px
-                } else{
+                } else {
                     return R.drawable.partly_cloudy_day_48px
                 }
 
             }
-            else ->{
+            else -> {
                 return R.drawable.cloudy_48px
             }
         }
     }
+
+    fun getWeatherDescription(id: Int): String {
+        val idStr = id.toString()[0].toString()
+        when (idStr) {
+            "2" -> {
+                return "Burza"
+            }
+            "3" -> {
+                return "Deszczyk"
+            }
+            "5" -> {
+                return "Deszcz"
+            }
+            "6" -> {
+                return "Śnieg"
+            }
+            "7" -> {
+                return "Zachmurzenie"
+            }
+            "8" -> {
+                if (id == 800) {
+                    return "Słonecznie"
+                } else {
+                    return "Częściowe zachmurzenie"
+                }
+
+            }
+            else -> {
+                return "Chmury"
+            }
+        }
+    }
+
+    fun temperatureCalculation(temp: Double){
+
+    }
+
+    fun getTemperatureUnit(){
+
+    }
+
 }
