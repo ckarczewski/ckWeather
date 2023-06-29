f0cf6452fde83b83c6e3543bf2f7d30b2cb3c4bf
Czarek Karczewski <keracz1997@gmail.com>
2023-06-29 18:08:17 +0200

    update database

--- app/src/main/java/com/example/ckweather/data/database/weather/WeatherDatabase.kt
+++ app/src/main/java/com/example/ckweather/data/database/weather/WeatherDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [WeatherItem::class, SettingItem::class], version = 2)
+@Database(entities = [WeatherItem::class, SettingItem::class], version = 3)
 abstract class WeatherDatabase: RoomDatabase(){
     abstract fun WeatherDao(): WeatherDao
     abstract fun SettingDao(): SettingDao
